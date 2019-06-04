e54ac4e1ae833b0fb6e266798d8721bfb99abb69
AlexG <alexgreau@live.fr>
2019-06-04 11:24:24 +0800

    getters and setters tacti

--- app/src/main/java/com/example/livecoaching/GameObjects/Tactic.java
+++ app/src/main/java/com/example/livecoaching/GameObjects/Tactic.java
@@ -4,8 +4,58 @@ public class Tactic {
 
     private String name;
     private String ID;
-    // type dans un enum
-    // sport dans un enum
+    private Sports sport;
+    private TacticTypes type;
     private int playersNeeded;
     private String creator;
+    // TODO : image
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public Sports getSport() {
+        return sport;
+    }
+
+    public void setSport(Sports sport) {
+        this.sport = sport;
+    }
+
+    public TacticTypes getType() {
+        return type;
+    }
+
+    public void setType(TacticTypes type) {
+        this.type = type;
+    }
+
+    public int getPlayersNeeded() {
+        return playersNeeded;
+    }
+
+    public void setPlayersNeeded(int playersNeeded) {
+        this.playersNeeded = playersNeeded;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
 }
