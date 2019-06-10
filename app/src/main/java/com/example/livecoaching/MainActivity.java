81b90ef83f370578900538e65699d8b85afd37f0
AlexG <alexgreau@live.fr>
2019-06-10 15:41:42 +0800

    toolbar in playActivity

--- app/src/main/java/com/example/livecoaching/MainActivity.java
+++ app/src/main/java/com/example/livecoaching/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar_main);
         setSupportActionBar(toolbar);
         // types spinner
         Spinner tacticFilter = findViewById(R.id.spinner_types);
--- app/src/main/java/com/example/livecoaching/PlayActivity.java
+++ app/src/main/java/com/example/livecoaching/PlayActivity.java
@@ -3,13 +3,16 @@ package com.example.livecoaching;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
 
 import com.example.livecoaching.Model.ApplicationState;
+import com.example.livecoaching.Model.Sequence;
 import com.example.livecoaching.Model.Tactic;
 
 public class PlayActivity extends AppCompatActivity {
     private Tactic tactic;
+    private Sequence sequence;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +25,9 @@ public class PlayActivity extends AppCompatActivity {
         tactic = ApplicationState.getInstance().getDisplayedList().get(index);
         TextView testText = (TextView) findViewById(R.id.testPlay);
         testText.setText(tactic.getName());
+
+        // toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar_play);
+        setSupportActionBar(toolbar);
     }
 }
