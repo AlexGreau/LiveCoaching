c6e1834a5578d999fc8c177b9a2aede8219b5a8f
AlexG <alexgreau@live.fr>
2019-06-18 17:06:55 +0800

    start of recycler view

---
+++ app/src/main/java/com/example/livecoaching/Adapter/ConnectedDevicesAdapter.java
@@ -0,0 +1,25 @@
+package com.example.livecoaching.Adapter;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class ConnectedDevicesAdapter {
+
+
+    class connectDevicesHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        public connectDevicesHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+
+        @Override
+        public void onClick(View v) {
+
+        }
+
+        public void bind (){
+            
+        }
+    }
+}
