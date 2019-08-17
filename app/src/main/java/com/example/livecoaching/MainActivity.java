2decfd08e30cd54b7c2f86ae272875e4767eccb8
AlexG <alexgreau@live.fr>
2019-08-17 22:48:27 +0800

    start of dialogs

--- app/src/main/java/com/example/livecoaching/MainActivity.java
+++ app/src/main/java/com/example/livecoaching/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.livecoaching;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -7,6 +10,8 @@ import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.livecoaching.Communication.Server;
 import com.example.livecoaching.Logs.Logger;
@@ -38,19 +43,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // init
-    protected void setFullScreen(){
-        this.getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                        // Set the content to appear under the system bars so that the
-                        // content doesn't resize when the system bars hide and show.
-                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        // Hide the nav bar and status bar
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
-    }
-
     protected void initLogger() {
         logger = new Logger(this);
         String ID = getIntent().getStringExtra("ID");
@@ -118,6 +110,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Log.d(TAG, "startButton pressed");
+                AlertDialog dialog = buildStartExpDialog();
+                dialog.show();
                 changeRunningStateTo(true);
             }
         });
@@ -129,7 +123,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Log.d(TAG, "startTestButton pressed");
-                changeRunningStateTo(true);
             }
         });
     }
@@ -140,8 +133,111 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Log.d(TAG, "finishButton pressed");
-                changeRunningStateTo(false);
+                AlertDialog dialog = buildFinnishDialog();
+                dialog.show();
             }
         });
+    }
+
+    // dialog functions
+    public AlertDialog buildStartExpDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this)
+                .setTitle("Start Experience")
+                .setMessage("Please enter the participant's ID below before continuing")
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        EditText ID = findViewById(R.id.IDparticipant);
+                        String IDtext = ID.getText().toString();
+                        if (!isValid(IDtext)) {
+                            startExp(IDtext);
+                        }
+                    }
+                })
+                // A null listener allows the button to dismiss the dialog and take no further action.
+                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        setFullScreen();
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setView(R.layout.startexp_dialog_layout);
+        return builder.create();
+    }
+
+    public AlertDialog buildFinnishDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this)
+                .setTitle("Finish Experience")
+                .setMessage("Are you sure you want to end this experience?")
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        finishExp();
+                    }
+                })
+                // A null listener allows the button to dismiss the dialog and take no further action.
+                .setNegativeButton(android.R.string.no, null)
+                .setIcon(android.R.drawable.ic_dialog_alert);
+
+        return builder.create();
+    }
+
+    public AlertDialog buildClearFileDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this)
+                .setTitle("Clear File")
+                .setMessage("Are you sure you want to Clear the log file and loose all the data ?")
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Continue with delete operation
+                        // TODO : clear file
+                    }
+                })
+
+                // A null listener allows the button to dismiss the dialog and take no further action.
+                .setNegativeButton(android.R.string.no, null)
+                .setIcon(android.R.drawable.ic_dialog_alert);
+
+        return builder.create();
+    }
+
+    // full screen function
+    protected void setFullScreen() {
+        this.getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+
+    // Logic functions
+    protected void startExp(String ID) {
+        // create new object exp(ID)
+        // exp.run()
+        changeRunningStateTo(true);
+        TextView test = (TextView) findViewById(R.id.testPlay);
+        test.setText(ID);
+    }
+
+    protected void finishExp() {
+        // get exp object
+        // exp.stop()
+        changeRunningStateTo(false);
+    }
+
+    protected void startTest() {
+        // create testRun object
+        // testRun.run()
+    }
+
+    protected boolean isValid(String text) {
+        // TODO : REGEX for no ":" or no special char
+        boolean isValid = true;
+
+
+        return isValid;
     }
 }
