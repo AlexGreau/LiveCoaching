731dfc88987a80b722bd25dc2684f81e61e8e739
AlexG <alexgreau@live.fr>
2019-08-17 16:54:36 +0800

    redoing Strating activity

--- app/src/main/java/com/example/livecoaching/StartingActivity.java
+++ app/src/main/java/com/example/livecoaching/StartingActivity.java
@@ -18,16 +18,7 @@ import android.widget.TextView;
 public class StartingActivity extends AppCompatActivity {
     // components
     TextView welcomeText;
-    EditText nameField;
-    CheckBox checkBox;
-    Spinner trajectory;
-    Spinner interaction;
     Button continueButton;
-
-    // values
-    String sequenceString = "All";
-    String interactionType = "All";
-    String testerName = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,9 +29,6 @@ public class StartingActivity extends AppCompatActivity {
 
     protected void init() {
         initText();
-        initNameField();
-        initCheckBox();
-        initSpinners();
         initButton();
     }
 
@@ -50,61 +38,18 @@ public class StartingActivity extends AppCompatActivity {
         welcomeText.setText(R.string.welcome_text);
     }
 
-    protected void initNameField() {
-        nameField = findViewById(R.id.nameField);
-
-    }
-
-    protected void initCheckBox() {
-        checkBox = findViewById(R.id.checkbox);
-        checkBox.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setSpinnersState(!checkBox.isChecked());
-            }
-        });
-    }
-
-    protected void initSpinners() {
-        // trajectory
-        trajectory = findViewById(R.id.trajectories);
-        ArrayAdapter<CharSequence> adapterTrajectories = ArrayAdapter.createFromResource(this,
-                R.array.trajectories, android.R.layout.simple_spinner_item);
-        adapterTrajectories.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        trajectory.setAdapter(adapterTrajectories);
-
-        // interaction
-        interaction = findViewById(R.id.interactionType);
-        ArrayAdapter<CharSequence> adapterInteractions = ArrayAdapter.createFromResource(this,
-                R.array.interactionTypes, android.R.layout.simple_spinner_item);
-        adapterInteractions.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        interaction.setAdapter(adapterInteractions);
-
-        // state
-        setSpinnersState(!checkBox.isChecked());
-    }
-
     protected void initButton() {
         continueButton = findViewById(R.id.continueButton);
         continueButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                testerName = nameField.getText().toString();
-                if (!testerName.equals("")) { // TODO : REGEX
-                    proceed();
-                } else {
-                    buildErrorDialog().show();
-                }
+                proceed();
+
             }
         });
     }
 
     protected void proceed() {
         startMainActivity();
-    }
-
-    protected void setSpinnersState(boolean bool) {
-        interaction.setEnabled(bool);
-        trajectory.setEnabled(bool);
     }
 
     protected AlertDialog buildErrorDialog() {
@@ -116,16 +61,12 @@ public class StartingActivity extends AppCompatActivity {
                 dialog.dismiss();
             }
         });
-
         AlertDialog dialog = builder.create();
         return dialog;
     }
 
     protected void startMainActivity() {
         Intent proceed = new Intent(this, MainActivity.class);
-        proceed.putExtra("ID", testerName);
-        proceed.putExtra("sequence", sequenceString);
-        proceed.putExtra("interactionType", interactionType);
         startActivity(proceed);
     }
 
