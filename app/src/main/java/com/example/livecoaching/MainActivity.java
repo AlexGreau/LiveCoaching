2746c6bc2633f6977f878868d261acf478d1bf8d
AlexG <alexgreau@live.fr>
2019-08-19 10:47:49 +0800

    minor state debug start exp

--- app/src/main/java/com/example/livecoaching/MainActivity.java
+++ app/src/main/java/com/example/livecoaching/MainActivity.java
@@ -125,7 +125,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Log.d(TAG, "startButton pressed");
-                changeRunningStateTo(true);
                 AlertDialog dialog = buildStartExpDialog();
                 dialog.show();
             }
