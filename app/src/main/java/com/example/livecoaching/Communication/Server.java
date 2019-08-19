52dfa3c25278d83cb54db07557ed15336ed83ba8
AlexG <alexgreau@live.fr>
2019-08-19 17:32:27 +0800

    transfer decode message to trial

--- app/src/main/java/com/example/livecoaching/Communication/Server.java
+++ app/src/main/java/com/example/livecoaching/Communication/Server.java
@@ -1,11 +1,5 @@
 package com.example.livecoaching.Communication;
 
-import android.location.Location;
-import android.location.LocationManager;
-
-import com.example.livecoaching.Interfaces.Decoder;
-import com.example.livecoaching.Logs.Logger;
-import com.example.livecoaching.Model.RouteCalculator;
 import com.example.livecoaching.Model.Trial;
 
 import java.io.DataInputStream;
@@ -13,9 +7,8 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
 
-public class Server implements Decoder {
+public class Server {
 
     protected final int PORT = 8080;
     protected Thread serverSocketThread;
@@ -23,96 +16,23 @@ public class Server implements Decoder {
     protected boolean running;
     private Trial trial;
 
-    protected Location actualLocation;
-    protected Logger logger;
-
     protected String messageFromClient;
     protected String replyMsg;
 
-    protected RouteCalculator routeCalculator;
-
     public Server(Trial trial) {
         this.trial = trial;
-        this.logger = trial.getLogger();
         serverSocketThread = new Thread(new SocketServerThread());
         running = true;
-        actualLocation = new Location(LocationManager.GPS_PROVIDER);
         serverSocketThread.start();
         System.out.println("Server launched");
     }
 
-    @Override
-    public String decodeMessage(String msg) {
-        // split message
-        String[] parts = msg.split(":");
-        String senderState = parts[0];
-        // interpret results
-        if (senderState.equals("Ready")) {
-            replyMsg = "continue:" + 2;
-            if (parts.length >= 2) {
-                logger.getLogsArray().clear();
-                parseInfos(parts[1]);
-                initRouteCalculator(logger.getLogsArray().get(0));
-            }
-        } else if (senderState.equals("Running")) {
-            System.out.println("detected " + senderState);
-            replyMsg = "";
-            if (parts.length >= 2) {
-                parseInfos(parts[1]);
-            }
-        } else if (senderState.equals("Stop")) {
-            System.out.println("detected " + senderState);
-            if (parts.length >= 2) {
-                parseInfos(parts[1]);
-            }
-            replyMsg ="";
-            stopLogging();
-        } else if (senderState.equals("End")) {
-            replyMsg = "reset";
-        } else if (senderState.equals("Asking")) {
-            parseInfos(parts[1]);
-            replyMsg = "route:" + format(routeCalculator.getActualRoute());
-        }
-
-        return replyMsg;
-    }
-
-    public void setRunning(boolean bool){
+    public void setRunning(boolean bool) {
         running = bool;
         if (!bool) {
             this.serverSocketThread.interrupt();
             this.serverSocketThread = null;
         }
-    }
-
-    private void parseInfos(String str) {
-        String[] infos = str.split("-");
-        actualLocation.setLatitude(Float.parseFloat(infos[0]));
-        actualLocation.setLongitude(Float.parseFloat(infos[1]));
-        logger.getLogsArray().add(actualLocation);
-        // System.out.println("added location to log : " + actualLocation);
-    }
-
-    private void stopLogging() {
-        System.out.println("stopping the logging");
-        System.out.println(logger.getLogsArray().size());
-        logger.flushLogArray();
-        logger.readLogFile();
-    }
-
-    private void initRouteCalculator(Location loc) {
-        routeCalculator = new RouteCalculator(loc, 0);
-        // routeCalculator.getRouteI();
-        System.out.println("route : " + routeCalculator.getActualRoute());
-    }
-
-    private String format(ArrayList<Location> locs) {
-        // formats the array of location into a sendable message
-        String res = "";
-        for (Location loc : locs) {
-            res += loc.getLatitude() + "-" + loc.getLongitude() + ";";
-        }
-        return res;
     }
 
     private class SocketServerThread extends Thread {
@@ -134,7 +54,7 @@ public class Server implements Decoder {
                     messageFromClient = dataInputStream.readUTF();
                     System.out.println("received message from client : " + messageFromClient);
 
-                    replyMsg = decodeMessage(messageFromClient);
+                    replyMsg = trial.decodeMessage(messageFromClient);
                     if (!replyMsg.isEmpty() || replyMsg != null) {
                         System.out.println("Sent : " + replyMsg);
                         dataOutputStream.writeUTF(replyMsg);
--- app/src/main/java/com/example/livecoaching/Model/Trial.java
+++ app/src/main/java/com/example/livecoaching/Model/Trial.java
@@ -1,9 +1,15 @@
 package com.example.livecoaching.Model;
 
+import android.location.Location;
+import android.location.LocationManager;
+
 import com.example.livecoaching.Communication.Server;
+import com.example.livecoaching.Interfaces.Decoder;
 import com.example.livecoaching.Logs.Logger;
 
-public class Trial  {
+import java.util.ArrayList;
+
+public class Trial implements Decoder {
     private Server server;
     private RouteCalculator routeCalculator;
 
@@ -13,12 +19,14 @@ public class Trial  {
     // logs
     private Logger logger;
 
+    // data
+    private Location actualLocation;
     private String participantID;
     private Double totalTime;
     private Double totalDistanceParcourue;
     private Double totalDistanceTheorique;
 
-    public Trial(String ID, int interactionType,int difficulty, Logger logger) {
+    public Trial(String ID, int interactionType, int difficulty, Logger logger) {
         this.logger = logger;
         this.participantID = ID;
         this.interactionType = interactionType;
@@ -31,12 +39,13 @@ public class Trial  {
         init();
     }
 
-    public void init(){
+    public void init() {
         logger.initNewLog(participantID, getInteractionString(interactionType));
+        actualLocation = new Location(LocationManager.GPS_PROVIDER);
         initRouteCalculator();
     }
 
-    public void initRouteCalculator(){
+    public void initRouteCalculator() {
 
     }
 
@@ -50,16 +59,83 @@ public class Trial  {
 
     }
 
+    @Override
+    public String decodeMessage(String msg) {
+        String replyMsg = "";
 
-        public Logger getLogger() {
+        // split message
+        String[] parts = msg.split(":");
+        String senderState = parts[0];
+        // interpret results
+        if (senderState.equals("Ready")) {
+            replyMsg = "continue:" + 2;
+            if (parts.length >= 2) {
+                logger.getLogsArray().clear();
+                parseInfos(parts[1]);
+                initRouteCalculator(logger.getLogsArray().get(0));
+            }
+        } else if (senderState.equals("Running")) {
+            System.out.println("detected " + senderState);
+            replyMsg = "";
+            if (parts.length >= 2) {
+                parseInfos(parts[1]);
+            }
+        } else if (senderState.equals("Stop")) {
+            System.out.println("detected " + senderState);
+            if (parts.length >= 2) {
+                parseInfos(parts[1]);
+            }
+            replyMsg = "";
+            stopLogging();
+        } else if (senderState.equals("End")) {
+            replyMsg = "reset";
+        } else if (senderState.equals("Asking")) {
+            parseInfos(parts[1]);
+            replyMsg = "route:" + format(routeCalculator.getActualRoute());
+        }
+
+        return replyMsg;
+    }
+
+    private void parseInfos(String str) {
+        String[] infos = str.split("-");
+        actualLocation.setLatitude(Float.parseFloat(infos[0]));
+        actualLocation.setLongitude(Float.parseFloat(infos[1]));
+        logger.getLogsArray().add(actualLocation);
+        // System.out.println("added location to log : " + actualLocation);
+    }
+
+    private void stopLogging() {
+        System.out.println("stopping the logging");
+        System.out.println(logger.getLogsArray().size());
+        logger.flushLogArray();
+        logger.readLogFile();
+    }
+
+    private String format(ArrayList<Location> locs) {
+        // formats the array of location into a sendable message
+        String res = "";
+        for (Location loc : locs) {
+            res += loc.getLatitude() + "-" + loc.getLongitude() + ";";
+        }
+        return res;
+    }
+
+    private void initRouteCalculator(Location loc) {
+        routeCalculator = new RouteCalculator(loc, 0);
+        // routeCalculator.getRouteI();
+        System.out.println("route : " + routeCalculator.getActualRoute());
+    }
+
+    public Logger getLogger() {
         return this.logger;
     }
 
-    public String getInteractionString(int type){
+    public String getInteractionString(int type) {
         String res;
-        if (type == 1){
+        if (type == 1) {
             res = "haptic";
-        } else if (type == 2){
+        } else if (type == 2) {
             res = "visual";
         } else {
             res = "haptic and visual";
